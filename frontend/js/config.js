770cc523a990c13e5c2766b43f00918777cc7067
noorullahumar <noorullahumary1@gmail.com>
2026-04-15 09:35:51 +0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://127.0.0.1:8000'
-        : 'https://api.yourdomain.com', // Change to your production API URL
+        : 'http://nu-ai-env.eba-ijpiuack.eu-north-1.elasticbeanstalk.com/', 
     DEBUG: window.location.hostname === 'localhost',
     SESSION_DURATION: 30 * 60 * 1000, // 30 minutes
     RATE_LIMIT_DELAY: 2000 // 2 seconds between submissions
